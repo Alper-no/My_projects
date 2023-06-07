4fcf3e189a875aa4f2e5c418715996860fc76e3a
Alper <alper.ccgn@gmail.com>
2023-06-07 22:11:41 +0200

    local

--- js_project- inntekt -utgift/app.js
+++ js_project- inntekt -utgift/app.js
@@ -12,6 +12,10 @@ const harcamaAlaniInput = document.getElementById("harcama-alani")
 const tarihInput = document.getElementById("tarih")
 const miktarInput = document.getElementById("miktar")
 
+//? Haracama Tablosu
+
+const harcamaBody = document.getElementById("harcama-body")
+
 //? Variables
 let gelirler = 0
 
@@ -40,6 +44,13 @@ window.addEventListener("load", () => {
   //? gelirler bilgisini local storage'dan okuyarak global degiskenimize yaz
   gelirler = Number(localStorage.getItem("gelirler"))
 
+  //? localStroge'den harcama listesini okuyarak global dizimize saklıyoruz.
+  harcamaListesi = JSON.parse(localStorage.getItem("harcamalar")) || []
+
+  //? harcama dizisinin icindeki objleri tek tek DOMa yaziyoruz.
+  harcamaListesi.forEach((harcama) => harcamayiDomaYaz(harcama))
+
+  console.log(harcamaListesi)
   //? Tarih inputunu bugun deger ile yukle
   tarihInput.valueAsDate = new Date()
 
@@ -53,7 +64,7 @@ harcamaFormu.addEventListener("submit", (e) => {
 
   //? yeni harcama bilgileri ile bir obje olusutur
   const yeniHarcama = {
-    id: new Date().getTime(),
+    id: new Date().getTime(), //? Sistem saatini (ms olarak) verir. Unique gibidir.
     tarih: tarihInput.value,
     alan: harcamaAlaniInput.value,
     miktar: miktarInput.value,
@@ -62,14 +73,51 @@ harcamaFormu.addEventListener("submit", (e) => {
   //? yeni harcama objesini diziye ekle
   harcamaListesi.push(yeniHarcama)
 
+  //? dizisin son halini localStorage e gonder.
+  localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi))
+
+  harcamayiDomaYaz(yeniHarcama)
+
   //? Formdaki verileri sil
   harcamaFormu.reset()
   tarihInput.valueAsDate = new Date()
-  console.log(yeniHarcama)
 })
-
 
 const hesaplaVeGuncelle = () => {
   gelirinizTd.innerText = gelirler
 }
+
+const harcamayiDomaYaz = ({ id, miktar, tarih, alan }) => {
+  // const { id, miktar, tarih, alan } = yeniHarcama
+  harcamaBody.innerHTML += `
+  <tr>
+    <td>${tarih}</td>
+    <td>${alan}</td>
+    <td>${miktar}</td>
+    <td><i id=${id} class="fa-solid fa-trash-can text-danger"  type="button"></i></td>
+  </tr>
+  `
+}
+
+harcamaBody.addEventListener("click", (e) => {
+  // console.log(e.target)
+
+  //? Event bir sil butonundan geldi ise
+  if (e.target.classList.contains("fa-trash-can")) {
+    //? DOM'dan ilgili row'u sildik.
+    e.target.parentElement.parentElement.remove()
+
+    const id = e.target.id
+    console.log(id)
+
+    //? Dizideki ilgili objeyi sildik.
+    harcamaListesi = harcamaListesi.filter((harcama) => harcama.id != id)
+
+    //? Silinmis yeni diziyi Local Storage aktardik.
+    localStorage.setItem("harcamalar", JSON.stringify(harcamaListesi))
+    
+    console.log(harcamaListesi)
+  }
+})
+
 

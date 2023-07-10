0b14d98f021fa5e3dc838348f58ff10048eb25ff
Alper <alper.ccgn@gmail.com>
2023-07-10 21:04:33 +0200

    onChance

--- nba/inclass/src/components/ContainerCard.jsx
+++ nba/inclass/src/components/ContainerCard.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import {data} from '../helpers/data';
 
 const ContainerCard = () => {
+  const [search, setSearch] = useState("");
+
+  const handleChange = (e) => {
+    setSearch(e.target.value) //!setter mtodlari her zaman asekron olarak calisir.
+    // console.log(search);
+  };
+
+  const filteredData = data.filter(item=> item.name.toLowerCase().includes(search.trim().toLowerCase()))
+
+  console.log(filteredData);
   return (
     <>
-      <Form.Control type="search" placeholder="Search Player..." />
-      <Container className='p-3 rounded-4 card-container my-3'>
-        <Row xs={2} md={4} lg={6} className='justify-content-center g-3 '>
+      <Form.Control
+        type="search"
+        placeholder="Search Player..."
+        onChange={handleChange}
+      />
+      <Container className="p-3 rounded-4 card-container my-3">
+        <Row xs={2} md={4} lg={6} className="justify-content-center g-3 ">
           <Col>1 of 2</Col>
           <Col>2 of 2</Col>
           <Col>3 of 3</Col>
-          
         </Row>
       </Container>
     </>
   );
-}
+};
 
-export default ContainerCard
+export default ContainerCard;

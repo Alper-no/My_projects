4c6834e95ed552597912c2fea8566d314d5dad82
Alper <alper.ccgn@gmail.com>
2023-07-10 21:30:33 +0200

    c3

--- nba/inclass/src/components/ContainerCard.jsx
+++ nba/inclass/src/components/ContainerCard.jsx
@@ -1,21 +1,25 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import {data} from '../helpers/data';
+import {data} from "../helpers/data";
+import PlayerCard from './PlayerCard';
 
 const ContainerCard = () => {
-  const [search, setSearch] = useState("");
+    const [search,setSearch] = useState("")
+    console.log(data)
+    const handleChange = (e) =>{
+        console.log(e.target.value)
+        setSearch(e.target.value) //! setter mtodları asenkron olarak çalışır.
+        // console.log(search)
+    }
+    console.log(search);
 
-  const handleChange = (e) => {
-    setSearch(e.target.value) //!setter mtodlari her zaman asekron olarak calisir.
-    // console.log(search);
-  };
+    const filteredData = data.filter(item=> item.name.toLowerCase().includes(search.trim().toLowerCase()))
 
-  const filteredData = data.filter(item=> item.name.toLowerCase().includes(search.trim().toLowerCase()))
+    console.log("filter",filteredData)
 
-  console.log(filteredData);
   return (
     <>
       <Form.Control
@@ -24,14 +28,28 @@ const ContainerCard = () => {
         onChange={handleChange}
       />
       <Container className="p-3 rounded-4 card-container my-3">
-        <Row xs={2} md={4} lg={6} className="justify-content-center g-3 ">
+        <Row xs={2} md={4} lg={6} className="justify-content-center g-3">
           <Col>1 of 2</Col>
-          <Col>2 of 2</Col>
-          <Col>3 of 3</Col>
+          {/* {filteredData.map((player, i) => (
+            <PlayerCard
+              key={i}
+              player={player}
+              img={player.img}
+              name={player.name}
+              statistics={player.statistics}
+              {...player}
+            />
+          ))} */}
+          {filteredData.map((player, i) => (
+            <PlayerCard
+              key={i}
+              {...player}
+            />
+          ))}
         </Row>
       </Container>
     </>
   );
-};
+}
 
-export default ContainerCard;
+export default ContainerCard

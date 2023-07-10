94d5e57e5e9180cb0365e87ad17d1b6d330a204c
Alper <alper.ccgn@gmail.com>
2023-07-10 20:37:13 +0200

    Container

---
+++ nba/inclass/src/components/ContainerCard.jsx
@@ -0,0 +1,11 @@
+import Form from 'react-bootstrap/Form';
+
+const ContainerCard = () => {
+  return (
+    <>
+    <Form.Control type="search" placeholder="Searc Player..." />
+    </>
+  )
+}
+
+export default ContainerCard;

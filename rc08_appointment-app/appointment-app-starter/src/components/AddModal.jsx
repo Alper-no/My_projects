04d0bf2485310fb18b023e1ac72936c22e5c319e
Alper <alper.ccgn@gmail.com>
2023-07-14 21:57:12 +0200

    paus

--- rc08_appointment-app/appointment-app-starter/src/components/AddModal.jsx
+++ rc08_appointment-app/appointment-app-starter/src/components/AddModal.jsx
@@ -1,10 +1,13 @@
-// import { useState } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 
 import Form from "react-bootstrap/Form";
 
 function AddModal({ show, handleClose }) {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState(new Date().);
+
   return (
     <>
       <Modal show={show} onHide={handleClose}>
@@ -15,12 +18,22 @@ function AddModal({ show, handleClose }) {
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Pasient Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Date</Form.Label>
-              <Form.Control type="date" placeholder="date" />
+              <Form.Control
+                type="date"
+                placeholder="date"
+                onChange={(e) => setDate(e.target.value)}
+                value={date}
+              />
             </Form.Group>
             <div className="text-center">
               <Button variant="success" type="submit" className="me-2">

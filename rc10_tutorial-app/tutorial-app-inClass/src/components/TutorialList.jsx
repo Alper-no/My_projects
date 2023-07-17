3b7ceff03d9ae06529d6bfcd3166f4116b234d66
Alper <alper.ccgn@gmail.com>
2023-07-17 21:45:08 +0200

    regulering

--- rc10_tutorial-app/tutorial-app-inClass/src/components/TutorialList.jsx
+++ rc10_tutorial-app/tutorial-app-inClass/src/components/TutorialList.jsx
@@ -1,19 +1,37 @@
 import { FaEdit } from "react-icons/fa"
 import { AiFillDelete } from "react-icons/ai"
+import axios from "axios"
+import Modal from "./Modal"
 
-const TutorialList = () => {
-  const tutorials = [
-    {
-      id: 1,
-      title: "JS",
-      description: "JS is a programming language",
-    },
-    {
-      id: 2,
-      title: "React",
-      description: "JS library for UI design",
-    },
-  ]
+const TutorialList = ({ tutorials, getTutorials }) => {
+  // const tutorials = [
+  //   {
+  //     id: 1,
+  //     title: "JS",
+  //     description: "JS is a programming language",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "React",
+  //     description: "JS library for UI design",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "VUE",
+  //     description: "JS library for UI design",
+  //   },
+  // ]
+
+  const handleDelete = async (id) => {
+    const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials"
+
+    try {
+      await axios.delete(`${BASE_URL}/${id}/`)
+    } catch (error) {
+      console.log(error)
+    }
+    getTutorials()
+  }
 
   return (
     <div className="container mt-4">
@@ -41,11 +59,14 @@ const TutorialList = () => {
                     size={20}
                     type="button"
                     className="me-2 text-warning"
+                    data-bs-toggle="modal"
+                    data-bs-target="#open-modal"
                   />
                   <AiFillDelete
                     size={22}
                     type="button"
                     className="text-danger "
+                    onClick={() => handleDelete(id)}
                   />
                 </td>
               </tr>
@@ -53,6 +74,8 @@ const TutorialList = () => {
           })}
         </tbody>
       </table>
+
+      <Modal />
     </div>
   )
 }
--- rc10_tutorial-app/tutorial-app-inClass/src/pages/Home.jsx
+++ rc10_tutorial-app/tutorial-app-inClass/src/pages/Home.jsx
@@ -1,24 +1,37 @@
-import { useState } from "react";
-import AddTutorial from "../components/AddTutorial";
-import TutorialList from "../components/TutorialList";
-import axios from "axios";
+import { useEffect, useState } from "react"
+import AddTutorial from "../components/AddTutorial"
+import TutorialList from "../components/TutorialList"
+import axios from "axios"
 
 const Home = () => {
-  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/";
-  const [tutorials, setTutorials]= useState([])
-  const getTutorials = async () => {
-    const data = await axios(BASE_URL)
-    
-    setTutorials(data.data)
-  };
+  const [tutorials, setTutorials] = useState([])
 
-  getTutorials()
+  const getTutorials = async () => {
+    const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
+    try {
+      // const res = await axios(BASE_URL)
+      // setTutorials(res.data)
+      const { data } = await axios(BASE_URL)
+      setTutorials(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  console.log(tutorials)
+
+  //? Mount asamasinda api'ye istek atiyoruz
+  useEffect(() => {
+    getTutorials()
+  }, [])
+
   return (
     <>
-      <AddTutorial />
-      <TutorialList />
+      <AddTutorial getTutorials={getTutorials} />
+      <TutorialList tutorials={tutorials} getTutorials={getTutorials} />
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
+

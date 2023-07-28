ad12c63ecfc9e96135c73adad89a7eec1ad61651
Alper <alper.ccgn@gmail.com>
2023-07-28 19:06:47 +0200

    HEader

--- rc15_clarus-library/starter/src/components/header/Header.jsx
+++ rc15_clarus-library/starter/src/components/header/Header.jsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import { HeaderButton, HeaderContainer, HeaderTitle, SearchInput, SelectBox,HeaderForm } from './Header.style'
+import React from "react";
+import {
+  HeaderButton,
+  HeaderContainer,
+  HeaderTitle,
+  SearchInput,
+  SelectBox,
+  HeaderForm,
+} from "./Header.style";
 
 const Header = () => {
   return (
-    <HeaderContainer>Header
+    <HeaderContainer>
+      Header
       <HeaderTitle>BOKKS OR MAGAZINES</HeaderTitle>
       <HeaderForm>
-        <SearchInput/>
-        <SelectBox> 
-          <option >ALL</option>
+        <SearchInput type="search" placeholder="Search" />
+        <SelectBox>
+          <option>ALL</option>
         </SelectBox>
         <HeaderButton>Search</HeaderButton>
       </HeaderForm>
     </HeaderContainer>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

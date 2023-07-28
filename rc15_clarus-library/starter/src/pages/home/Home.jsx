ba8c764b06aa4ac3e6a4cc30ef3160ab399b7d86
Alper <alper.ccgn@gmail.com>
2023-07-28 18:41:45 +0200

    Home

--- rc15_clarus-library/starter/src/pages/home/Home.jsx
+++ rc15_clarus-library/starter/src/pages/home/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/header/Header";
+import Card from "../../components/card/Card";
+import { CardContainer, HomeContainer, HomeImage } from "./Home.style";
+import homeImg from "../../assets/books.jpg";
+// import { useBooksContext } from "../../context/BooksContext";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <HomeContainer>
+      <Header />
+      <HomeImage>
+        <img src={homeImg} alt="Home"/>
+      </HomeImage>
+      <CardContainer>
+        <Card />
+      </CardContainer>
+    </HomeContainer>
+  );
+};
 
-export default Home
+export default Home;

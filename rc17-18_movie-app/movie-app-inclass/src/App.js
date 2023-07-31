014dd57f8f65faa79884cb77b15da8fb20f43d23
Alper <alper.ccgn@gmail.com>
2023-07-31 20:36:09 +0200

    Navbar2

--- rc17-18_movie-app/movie-app-inclass/src/App.js
+++ rc17-18_movie-app/movie-app-inclass/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
+// import Register from "./pages/Register";
+// import Navbar from "./components/Navbar";
+import AppRouter from "./router/AppRouter";
 
 const App = () => {
   return (
-    <div>
-      <Register />
-      <Navbar/>
+    <div className="dark:bg-gray-dark-main min-h-screen ">
+      <AppRouter/>
     </div>
   );
 };

e28dafc2062868808ea6a7b1e258129c6e0bc8b4
Alper <alper.ccgn@gmail.com>
2023-07-31 19:52:57 +0200

    Navbar2

--- rc17-18_movie-app/movie-app-inclass/src/App.js
+++ rc17-18_movie-app/movie-app-inclass/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
       <Register />
+      <Navbar/>
     </div>
   );
 };
